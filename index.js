7fed0f6b0d9a69f25223cd2230f33015912b8f36
Muskan Saini <muskansaini@Muskans-MacBook-Air.local>
2024-12-26 12:15:23 +0530

    change the post local to live

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ require("./src/db/connect");
 const routes = require("./src/router/routes");  
 
 
-const PORT = 3000;
+const PORT = 'https://portfolio-backend-aj8i.onrender.com';
 const app = express();
 app.use(express.json());
 app.use("/api/v1", routes);
